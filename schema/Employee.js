db85640cb5306b2d33a23ceb53b95e18fa31dc0a
Waffles <knharman@gmail.com>
2022-02-04 17:09:35 -0800

    CRUD methods for Employee

--- schema/Employee.js
+++ schema/Employee.js
@@ -6,17 +6,44 @@ class Employee {
         this.managerId = managerId;
     }
 
-    create() {
-
+    async create() {
+        const [results] = await this.connection.execute('INSERT INTO employee (first_name, last_name, job_id, manager_id) VALUES (?,?,?,?)', 
+            [this.firstName, this.lastName, this.jobId, this.managerId])
+        this.id = results.insertId
+        console.log("successfully created record")
     }
-    update() {
 
+    async update(updatedObject) {
+        const {firstName, lastName, jobId, managerId} = updatedObject
+
+        await this.connection.execute('UPDATE employee SET first_name=(?), last_name=(?), job_id=(?), manager_id=(?) WHERE id=(?)',
+            [firstName, lastName, jobId, managerId, this.id])
+
+        this.firstName = firstName
+        this.lastName = lastName
+        this.jobId = jobId
+        this.managerId = managerId
+
+        console.log("successfully updated record")
     }
-    read() {
 
+    async read() {
+        let results = await this.connection.execute('SELECT * FROM employee WHERE id=(?)', [this.id])
+        
+        const {firstName, lastName, jobId, managerId, id} = results[0][0]
+
+        this.id = id
+        this.firstName = firstName
+        this.lastName = lastName
+        this.jobId = jobId
+        this.managerId = managerId
+
+        console.log('successfully read row')
     }
-    delete() {
 
+    async remove() {
+        await this.connection.execute('DELETE FROM employee WHERE id=(?)', [this.id])
+        console.log('successfully deleted row')
     }
 }
 
